aa736550f3b4ceddd0e4cbead2caa78d0be3a9fc
leehyunjun <zxzxs9182@gmail.com>
2019-03-25 19:54:41 +0900

    sonarqube테스트입니다.

--- src/main/java/hello/SampleController.java
+++ src/main/java/hello/SampleController.java
@@ -68,6 +68,7 @@ public class SampleController {
                 System.out.println("안녕하세요");
                 System.out.println("슬랙테스티입니다.");
                 System.out.println("슬랙테스티입니다.");
+                System.out.println("슬랙테스티입니다.22");
                 System.out.println("올바른 명령어를 입력하세요");
             }
 
