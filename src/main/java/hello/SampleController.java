f438d8f360002235ff77db8b2d899b9ee0cd3319
leehyunjun <zxzxs9182@gmail.com>
2019-03-25 21:24:04 +0900

    pure

--- src/main/java/hello/SampleController.java
+++ src/main/java/hello/SampleController.java
@@ -9,7 +9,6 @@ import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-
 @Controller
 @EnableAutoConfiguration
 public class SampleController {
@@ -35,64 +34,66 @@ public class SampleController {
 
         int button;
 
-        logger.log(Level.INFO, "사용자가 가지고있는액수");
+        logger.log(Level.INFO,"사용자가 가지고있는액수");
 
         while (!end) {
             printMenu();
             button = in.nextInt();
             printMenu();
             if (userMoney < 0) {
-
-                logger.log(Level.INFO, "액수가 부족합니다.");
+                System.out.println("액수가 부족합니다.");
                 end = true;
             }
-            if(button == 1 || button ==4) {
+            if (button == 1) {
                 totalPrice += calcTotalPrice(1000, 1);
                 userMoney = calcUserMoney(userMoney, totalPrice);
             }
             else if (button == 2) {
                 totalPrice += calcTotalPrice(1500, 1);
                 userMoney = calcUserMoney(userMoney, totalPrice);
-            } else if (button == 3) {
+            }
+            else if (button == 3) {
                 totalPrice += calcTotalPrice(1300, 1);
                 userMoney = calcUserMoney(userMoney, totalPrice);
             }
-             else if (button == 0) {
+            else if (button == 4) {
+                totalPrice += calcTotalPrice(1000, 1);
+                userMoney = calcUserMoney(userMoney, totalPrice);
+            }
+            else if (button == 0) {
                 end = true;
+                System.out.println("구매금액 : " + totalPrice + " 잔액 : " + userMoney);
+                System.out.println("이용해주셔서 감사합니다.");
+            }
+            else {
 
-               logger.info("액수가 부족합니다.");
-
-
-            } else {
-                logger.log(Level.INFO, "올바른 명령어를 입력하세요");
-
+                System.out.println("올바른 명령어를 입력하세요");
             }
 
-
         }
+
+
+
     }
+
     /*
      * calculate userMoney and totalPrice Method
      * if userMoney bigger than totalPrice, return positive numbers
      * else return negative numbers.
-    */
+     */
 
     private static int calcUserMoney(int userMoney, int totalPrice) {
         return userMoney - totalPrice;
     }
 
     private static void printMenu(){
-        Logger logger = Logger.getLogger(SampleController.class.getName());
-
-        logger.log(Level.INFO,"---------------------------");
-        logger.log(Level.INFO,"1.콜라 1000");
-        logger.log(Level.INFO,"2.환타 1500");
-        logger.log(Level.INFO,"3.사이다 1300");
-        logger.log(Level.INFO,"4.포카리스웨트 1000");
-        logger.log(Level.INFO,"5.조지아 1600");
-        logger.log(Level.INFO,"---------------------------");
-
-
+        System.out.println("---------------------------");
+        System.out.println("1.콜라 1000");
+        System.out.println("2.환타 1500");
+        System.out.println("3.사이다 1300");
+        System.out.println("4.포카리스웨트 1000");
+        System.out.println("5.조지아 1600");
+        System.out.println("---------------------------");
     }
 
     public static int calcTotalPrice(int price, int number){
